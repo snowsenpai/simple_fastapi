bf2b512c2a7543d9d3b4fe3f9aefb52ca9ba4c5c
Umoh Samuel <samuelwisdom360@gmail.com>
2024-07-13 16:40:00 +0100

    fix typo in jwt decode argument

--- app/oauth2.py
+++ app/oauth2.py
@@ -20,13 +20,14 @@ def create_access_token(data: dict):
 
 def verify_access_token(token: str, credentials_exception):
     try:
-        payload = jwt.decode(token, settings.jwt_secret_key, algorithm=settings.jwt_algorithm)
+        payload = jwt.decode(token, settings.jwt_secret_key, algorithms=settings.jwt_algorithm)
         id = payload.get("user_id")
         if id is None:
             raise credentials_exception
 
         token_data = schemas.TokenData(id=id)
     except JWTError as e:
+        print(e)
         raise credentials_exception
     return token_data
     
