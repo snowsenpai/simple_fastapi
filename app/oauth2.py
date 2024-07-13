51cb4add3c682535f9bc3fdb9e85ef9cc9e6773f
Umoh Samuel <samuelwisdom360@gmail.com>
2024-07-13 19:31:16 +0100

    return user data using decoded access token's id field

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,9 @@
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
+from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 from .config import settings
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
@@ -31,6 +32,12 @@ def verify_access_token(token: str, credentials_exception):
         raise credentials_exception
     return token_data
     
-def get_current_user(token: str = Depends(oauth2_schema)):
+def get_current_user(token: str = Depends(oauth2_schema), db: Session = Depends(database.get_db)) -> models.User:
     credentials_exception = HTTPException(status.HTTP_401_UNAUTHORIZED, "Could not validate credentials", {"WWW-Authenticate":"Bearer"})
-    return verify_access_token(token, credentials_exception)
+    token_data = verify_access_token(token, credentials_exception)
+
+    user = db.query(models.User).filter(models.User.id == token_data.id).first()
+    if not user:
+        # valid token but user record is not in db
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "authenticated user not found")        
+    return user
