b39352e00104369cab5a9f9c097d94bb61aef2ad
Umoh Samuel <samuelwisdom360@gmail.com>
2024-07-15 00:31:00 +0100

    add sql file for referencing join query to include the number of posts a user has created

---
+++ query.sql
@@ -0,0 +1,11 @@
+-- include the number of posts each user has created
+SELECT users.id AS user_id, email, COUNT(posts.owner_id) as num_of_posts
+FROM users
+LEFT JOIN posts ON posts.owner_id = users.id
+GROUP BY users.id;
+
+-- with right join, just for sql practice
+-- SELECT users.id AS user_id, users.email AS user_email, COUNT(posts.id) AS num_of_posts
+-- FROM posts 
+-- RIGHT JOIN users ON posts.owner_id = users.id
+-- GROUP BY users.id;
